7df22d7cd55e5adbc4e7148a56fdc91d2bbc0028
Aditya Herwade <adityaherwade17@gmail.com>
2026-04-05 15:20:51 +0530

    Update App.py

--- App.py
+++ App.py
@@ -12,9 +12,9 @@ import matplotlib.pyplot as plt
 # LOAD DATA
 @st.cache_data
 def load_data():
-    matches = pd.read_csv("https://raw.githubusercontent.com/datasets/ipl/master/data/matches.csv")
-    deliveries = pd.read_csv("https://raw.githubusercontent.com/datasets/ipl/master/data/deliveries.csv")
-    
+    matches = pd.read_csv("https://raw.githubusercontent.com/akshaykumar6/ipl-data/master/matches.csv")
+    deliveries = pd.read_csv("https://raw.githubusercontent.com/akshaykumar6/ipl-data/master/deliveries.csv")
+
     matches['date'] = pd.to_datetime(matches['date'])
     return matches, deliveries
 
